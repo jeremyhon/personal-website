fc60585cfd6ed46ece53af70d2e789c59ee23a99
Jeremy Hon <jeremy.hon.gy@gmail.com>
2026-01-04 16:15:58 +0800

    remove unnecessary content

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-		}),
-});
-
 const home = defineCollection({
 	loader: glob({ base: './src/content/home', pattern: '**/*.{md,mdx}' }),
 	schema: z.object({
@@ -23,4 +8,4 @@ const home = defineCollection({
 	}),
 });
 
-export const collections = { blog, home };
+export const collections = { home };
